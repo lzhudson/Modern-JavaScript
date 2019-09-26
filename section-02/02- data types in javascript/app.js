c723727ebc695f27d6a4e69ce099541454acff76
Hudson Holanda <hudsonholanda55@gmail.com>
2019-09-26 09:42:21 -0300

    Adicionando os arquivos referentes a segunda aula que é sobre tipos de dadoa em javascript.

---
+++ section-02/02- data types in javascript/app.js
@@ -0,0 +1,46 @@
+// PRIMITIVE
+
+// String
+const name = 'Jhon Doe';
+// typeof = return type variable.
+console.log(typeof name);
+
+// Number
+const age = 45;
+console.log(typeof age);
+
+// Boolean
+const hasKids = true;
+console.log(typeof hasKids); 
+
+// Null
+const car = null;
+console.log(typeof car);
+
+// Undefined
+let test;
+console.log(typeof test);
+
+// Symbol
+const symbol = Symbol();
+console.log(typeof symbol);
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'music'];
+console.log(typeof hobbies);
+
+// Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
+}
+console.log(typeof address);
+
+const today = new Date();
+console.log(today);
+console.log(typeof today);
+
+
+
+
---
+++ section-02/02- data types in javascript/readme-lesson.txt
@@ -0,0 +1,22 @@
+Tipos de dados em Javascript:
+Tipos de dados Primitivos
+  String
+  Number
+  Boolean
+  Null
+  undefined
+  Symbol
+
+Tipo de dados referênciais:
+  Arrays
+  Object Literals
+  Functions
+  Date
+  Entre outros...
+
+Tipada Dinamicante = Uma variavel por ser tipada dinamicamente ou seja seu valor pode ser associado a uma string e em seguida a um numero.
+Para tipar estatiamente é necessario usar uma linguagem chamada typescript.
+
+Operador typeof = Retorna o tipo daquela variavel.
+Ex: let name = 'Hudson';
+    typeof name retorna String
