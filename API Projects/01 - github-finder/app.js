d12985e9cf90df11a74fd6bfcb2198f3de8cb253
Hudson Holanda <hudsonholanda55@gmail.com>
2020-02-14 23:27:14 -0200

    adicionando a classe ui e o layout do perfil para exibição.

--- API Projects/01 - github-finder/app.js
+++ API Projects/01 - github-finder/app.js
@@ -1,19 +1,26 @@
 // Search input
 const searchUser = document.getElementById('searchUser');
 
+// Init Github
+const github = new Github();
+
+// Init UI
+const ui = new UI();
+
 // Search input event listener
 searchUser.addEventListener('keyup', (e) => {
   // Get input text
   const userText = e.target.value;
 
-  const github = new Github();
+ 
   if(userText !== '') {
     github.getUser(userText)
-          .then(data => {
+          .then(data => { 
             if(data.profile.message === 'Not Found') {
               // Show alert
             } else {
               // Show profile
+              ui.showProfile(data.profile);
             }
           })
   } else {
